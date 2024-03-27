f631913a0b4175fc7f7616e014652936d36bc276
”kodokunawa” <kodokunawa@gmail.com>
2024-03-27 16:19:06 +0300

    feat: added routes

---
+++ pages/bookmarked/index.tsx
@@ -0,0 +1,7 @@
+function Bookmarked() {
+    return(
+        <h1>Bookmarked Page</h1>
+    )
+}
+
+export default Bookmarked;
---
+++ pages/tv-series/index.tsx
@@ -0,0 +1,7 @@
+function TVSeries(){
+    return(
+        <h1>TV Series Page</h1>
+    )
+}
+
+export default TVSeries;
