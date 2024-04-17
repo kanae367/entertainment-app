b918a0a43c9278cb9c4531aa7c87105061cc5701
”kodokunawa” <kodokunawa@gmail.com>
2024-04-17 14:18:23 +0300

    feat: tablet styles for header

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,8 +1,8 @@
 "use client"
-import Image from "next/image";
 import classes from './header.module.scss';
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import Image from 'next/image';
 const nav = ['home', 'bookmark', 'movies', 'tv-series'];
 
 const Header = (props: {hidden: boolean}) => {
@@ -12,21 +12,21 @@ const Header = (props: {hidden: boolean}) => {
 
     return(
         <header hidden={hidden} className={classes.header}>
-            <Image width={25} height={20} src="/assets/logo.svg" alt="logo" />
+            <Image width={25} height={20} className={classes.logo} src="/assets/logo.svg" alt="logo" />
             <nav className={classes.navigation}>
                 <ul className={classes.list}>
 
                     {nav.map(item => 
                         <li key={item} className={item === current || (item === 'home' && !current)  ? classes.active : classes.item} >
                             <Link href={item === "home" ? "/" : "/" + item}>
-                                <Image width={16} height={16} src={`/assets/icon-nav-${item}.svg`} alt={item}></Image>
+                                <Image width={16} height={16} className={classes.navImage} src={`/assets/icon-nav-${item}.svg`} alt={item}></Image>
                             </Link>
                         </li>
                     )}
                 </ul>
             </nav>
             <button className={classes.profile}>
-                <Image width={24} height={24}className={classes.profileImage} src="/assets/image-avatar.png" alt="Profile Picture"></Image>
+                <Image width={24} height={24} className={classes.profileImage} src="/assets/image-avatar.png" alt="Profile Picture"></Image>
             </button>
         </header>
     )
